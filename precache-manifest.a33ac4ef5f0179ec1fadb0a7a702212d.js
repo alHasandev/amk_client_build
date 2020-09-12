307700ec9600ccfde3b078bc195cbd751941b8c6
alHasandev <alhasan99dev@gmail.com>
2020-09-13 00:44:27 +0800

    initial commit

---
+++ precache-manifest.a33ac4ef5f0179ec1fadb0a7a702212d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b6d4c615450b15423a4785d6ed41ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34b43922a81d079d8c7a",
+    "url": "/static/css/main.6a04644e.chunk.css"
+  },
+  {
+    "revision": "bda5ab8298532d6f2d9c",
+    "url": "/static/js/2.5cd30713.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5cd30713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b43922a81d079d8c7a",
+    "url": "/static/js/main.d98ecc16.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "efd762d0fb8b481ecfa40fad75d79f0a",
+    "url": "/static/media/lobby.efd762d0.jpg"
+  },
+  {
+    "revision": "364a243ab278871efa63dcec19149d88",
+    "url": "/static/media/logo-frontweb.364a243a.png"
+  },
+  {
+    "revision": "cd8469bcb98852ee6b4d5772aaee3d78",
+    "url": "/static/media/user-girl-t-shirt.cd8469bc.png"
+  }
+]);
